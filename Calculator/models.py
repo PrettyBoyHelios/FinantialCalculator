34fb00a083ac166f17b9945b37c7ef5264bec12a
helios <helios@polispay.org>
2018-11-13 22:29:51 -0600

    Added initial alternative model

--- Calculator/models.py
+++ Calculator/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+INTEREST_TYPE = (
+    ('period','p'),
+    ('effective', 'e'),
+    ('nominal','n'),
+)
+class Alternative(models.Model):
+    interest = models.DecimalField(max_digits=4, decimal_places=2)
+    interest_type = models.CharField(max_length=10, choices=INTEREST_TYPE, default='nominal')
+
--- FinCalc/settings.py
+++ FinCalc/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Calculator.apps.CalculatorConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
