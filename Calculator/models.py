bbd157f1b307ba1882340daa2eccd3df42fdd7f5
helios <helios@polispay.org>
2018-11-14 23:35:17 -0600

    Updated navbar content, alternatives model and form and added initial alternatives list view method

--- Calculator/models.py
+++ Calculator/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.forms import ModelForm
 
 # Create your models here.
 INTEREST_TYPE = (
@@ -7,6 +8,14 @@ INTEREST_TYPE = (
     ('nominal','n'),
 )
 class Alternative(models.Model):
+    name = models.CharField(max_length=50)
     interest = models.DecimalField(max_digits=4, decimal_places=2)
     interest_type = models.CharField(max_length=10, choices=INTEREST_TYPE, default='nominal')
+    operative_costs = models.DecimalField(max_digits=11, decimal_places=2)
 
+
+
+class AlternativeForm(ModelForm):
+    class Meta:
+        model = Alternative
+        fields = ['name', 'interest','interest_type']
--- Calculator/views.py
+++ Calculator/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpRespose
+from Calculator.models import AlternativeForm
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Index!")
+    form = AlternativeForm()
+    return render(request, "Calculator/index.html", {'form': form})
+
+def alternatives_list(request):
+
+    return render(request, "Calculator/alternatives_list.html")
