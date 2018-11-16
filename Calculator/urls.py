48cade414b94d650c9d9385960d3c0632af77a5e
helios <helios@polispay.org>
2018-11-15 21:40:31 -0600

    Actual changes for previus commit

--- Calculator/urls.py
+++ Calculator/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('alt/', views.alternatives_list, name="alternatives"),
+    path('alt/create/', views.create_alternative, name="create_alternative"),
 ]
